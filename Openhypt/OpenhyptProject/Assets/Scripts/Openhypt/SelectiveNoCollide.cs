426f83b3a9da070a6fb916b4c59ba452103c54b3
BioQuake <22009334+BioQuake@users.noreply.github.com>
2020-10-10 00:09:46 +0200

    First few collision models are added. Check the spreadsheet for info

---
+++ Openhypt/OpenhyptProject/Assets/Scripts/Openhypt/SelectiveNoCollide.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectiveNoCollide : MonoBehaviour
+{
+
+    public Collider[] ignoreColliders;
+    public Collider[] localColliders;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0;i<ignoreColliders.Length;i++)
+        {
+            for(int j=0;j<localColliders.Length;j++)
+            {
+                Physics.IgnoreCollision(localColliders[j], ignoreColliders[i], true);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
